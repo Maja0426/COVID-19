cbd3084c95dc42b1b496bf8ddbdfaeebeb665c1d
Majoros Tamás <majorost@pop-os.localdomain>
2020-03-26 23:55:02 +0100

    delete share btn

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -180,11 +180,11 @@
           Szerző: {post.name}
         </h5>
       </div>
-      <div
+      <!-- <div
         class="fb-share-button"
         data-href="https://bgykaranten.hu/blog/{slug}"
         data-layout="button"
-        data-size="small" />
+        data-size="small" /> -->
     </div>
   </div>
   <div class="content">
