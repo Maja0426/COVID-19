0cd3b5c3e6a4d8fd0ea3b8a5493e8e2a1733b2c4
Majoros Tamás <majorost@pop-os.localdomain>
2020-04-25 10:28:25 +0200

    refresh map

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -438,7 +438,7 @@
 
   <div class="corona-map">
     <img
-      use:lazy={{ src: 'https://koronavirus.gov.hu/sites/default/files/terkep0424.jpg' }}
+      use:lazy={{ src: 'https://koronavirus.gov.hu/sites/default/files/terkep0425.jpg' }}
       alt="megyénkénti koronavírus statisztika" />
   </div>
 
